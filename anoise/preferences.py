c1d4beb01d122ca8a7053862256da2c98755e2a9
Fernando Mussel <fernandomussel91@gmail.com>
2016-09-28 21:25:14 -0300

    fix: Create autostart dir if it does not exist
    Taking the patch from https://github.com/mussel/anoise/commit/1f48f98d472cb316adcab4f3b5c18793e4e03957
    However, edited it to work with the XDG patches already applied.
    
    Author:    Fernando Mussel <fernandomussel91@gmail.com>
    Date:      Wed Sep 28 21:25:14 2016 -0300

--- anoise/preferences.py
+++ anoise/preferences.py
@@ -31,7 +31,8 @@ _ = gettext.gettext
 class Preferences:
     """This will be for DE as MATE 14.10+ which hasn't sound indicator with Gtk3"""
     def __init__(self, player):
-        self.AUTOSTART = os.path.join(BaseDirectory.xdg_config_home, 'autostart', 'anoise.desktop')
+        self.AUTOSTART_DIR = os.path.join(BaseDirectory.xdg_config_home, 'autostart')
+        self.AUTOSTART = os.path.join(self.AUTOSTART_DIR, 'anoise.desktop')
         self.DATA_DIR  = os.path.join(BaseDirectory.xdg_data_home, 'anoise')
         self.DESKTOP = '/usr/share/applications/anoise.desktop'
         
@@ -49,8 +50,11 @@ class Preferences:
         self.btn_noises   = builder.get_object('btn_show_noises')
         self.web          = builder.get_object('boxWeb')
         
-        # Autostart
-        if os.path.isfile(self.AUTOSTART):
+        if not os.path.isdir(self.AUTOSTART_DIR):
+            # autostart dir does not exits. Create it
+            os.makedirs(self.AUTOSTART_DIR)
+        elif os.path.isfile(self.AUTOSTART):
+            # autostart dir exists and autostart file is present
             self.cb_autostart.set_active(True)
         else:
             self.cb_autostart.set_active(False)
