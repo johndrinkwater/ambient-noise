f91467b026c9444f73f95f1bda6912d26bdb79b9
John Drinkwater <john@nextraweb.com>
2018-08-13 12:05:54 +0100

    Support basic media key usage
    Play, Pause, Stop, Next, Prev are supported.
    
    Not aware of the support for this across distros, but we shall see.

--- anoise/anoise.py
+++ anoise/anoise.py
@@ -22,6 +22,8 @@ from six.moves import urllib
 gi.require_version('Gtk', '3.0')
 gi.require_version('Gst', '1.0')
 from gi.repository import Gtk, GObject, Gst
+gi.require_version('Keybinder', '3.0')
+from gi.repository import Keybinder
 from dbus.mainloop.glib import DBusGMainLoop
 from utils import *
 from sound_menu import SoundMenuControls
@@ -58,6 +60,20 @@ class ANoise:
         self.noise = Noise()
         self.win_preferences = Preferences(self)
 
+        try:
+            self.keybinder = Keybinder
+            self.keybinder.init()
+            if self.keybinder.bind('AudioPlay', self._sound_menu_play, None):
+                self.keybinder.bind('AudioStop', self._sound_menu_pause, None)
+                self.keybinder.bind('AudioPause', self._sound_menu_pause, None)
+                self.keybinder.bind('AudioNext', self._sound_menu_next, None)
+                self.keybinder.bind('AudioPrev', self._sound_menu_previous, None)
+            else:
+                self.keybinder = None
+
+        except (ValueError, ImportError):
+            self.keybinder = None
+
         # Need in a few DE
         try:
             self.window = GUI(self)
@@ -88,7 +104,7 @@ class ANoise:
         """Called in the first click"""
         return self.is_playing
 
-    def _sound_menu_play(self):
+    def _sound_menu_play(self, keypress = None, data = None):
         """Play"""
         self.is_playing = True # Need to overwrite this for an issue with autstart
         self.sound_menu.song_changed(self.noise.get_current_index(), '', '', self.noise.get_name(),
@@ -98,7 +114,7 @@ class ANoise:
         self.player.set_state(Gst.State.PLAYING)
         self.sound_menu.signal_playing()
 
-    def _sound_menu_pause(self):
+    def _sound_menu_pause(self, keypress = None, data = None):
         """Pause"""
         self.is_playing = False # Need to overwrite this for an issue with autstart
         self.player.set_state(Gst.State.PAUSED)
@@ -121,11 +137,11 @@ class ANoise:
                 urllib.parse.quote(self.noise.get_icon_uri(), ':/'),
                 urllib.parse.quote(self.noise.get_current_filename_uri(), ':/'))
 
-    def _sound_menu_previous(self):
+    def _sound_menu_previous(self, keypress = None, data = None):
         """Previous"""
         self._set_new_play('previous')
 
-    def _sound_menu_next(self):
+    def _sound_menu_next(self, keypress = None, data = None):
         """Next"""
         self._set_new_play('next')
 
